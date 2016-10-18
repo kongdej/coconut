087fb4983400176492fab4aed53f7c94d086e6cd
kongdej <kongdejs@gmail.com>
2016-10-18 19:26:48 +0700

    no message

--- db.js
+++
@@ -1,9 +0,0 @@
-
-var mongojs = require('mongojs');
-var databaseurl = 'tkpdb';
-var collections = ['solarplant','solarroof'];
-var connect = mongojs(databaseurl,collections);
-
-module.exports = {
-	connect: connect
-}
