f0faccb0d02c9de97189b9c5a3bcf8dc234c89b3
EGAT-kongdej <kongdejs@gmail.com>
2016-11-22 15:49:13 +0700

    no message

--- getData.js
+++ getData.js
@@ -4,7 +4,6 @@ var mysql = require('mysql');
 var dateFormat = require('dateformat');
 var oid = require('./config/oidtag');
 
-var fileName = "roof/solarroof.csv";
 var db = mongojs.connect;
 var connection = mysql.createConnection({
 /*
@@ -22,15 +21,20 @@ var connection = mysql.createConnection({
 connection.connect();
 
 //var port = process.env.PORT || 2016
-var request_time = 30000; // 30 seconds
+var request_time = 2000; // 30 seconds
 
 var sql  = "SELECT attr_oid,fValue,from_unixtime(attr_time,'%Y-%m-%d %H:%i') modified FROM ";
 
-//select table yyyymm
-//var now = new Date();
+/********************************************************************/
+var now = new Date();
 //sql += 'analogueother'+dateFormat(now,"yyyymm");
 sql += 'analogueother201610';
 
+//var fileName = "./roof/20161118.csv";
+var fileName = "P:/solarRoof/20160922.csv";
+//var fileName = './roof/' + dateFormat(now,"yyyymmdd")+'.csv';
+
+/********************************************************************/
 sql += ' WHERE attr_oid in ';
 var idlist=[];
 for (var k in oid.name) {
@@ -66,32 +70,42 @@ var interval = setInterval(function() {
 	  //var date = new Date();
       //console.log(rows);
 
- 	  db.solarplant.remove({});
-
+      /* get solar plant data */
 	  var datalist=[];
 	  for (var i=0; i < rows.length; i++) { 
 	  	datalist[i] = new dataDetailobj(rows[i].attr_oid,oid.name[rows[i].attr_oid],rows[i].fValue,oid.unit[rows[i].attr_oid]);
 	  };
+	  
+	 
 	  var result = new dataobj("Solar Plant",rows[0].modified,datalist);
+
+ 	  db.solarplant.remove({});
  	  db.solarplant.insert(result);
+ 	  console.log('GET Solar Plant @'+rows[0].modified);
 
-
+ 	  /* get solar roof data*/
 	  fs.readFile(fileName, 'utf-8', function(err, data) {
 		    if (err) throw err;
 
 		    var lines = data.trim().split('\n');
+		    var firstLine = lines[9];
 		    var lastLine = lines.slice(-1)[0];
-
 		    var fields = lastLine.split(';');
+		    var fields_first = firstLine.split(';');
 
+		   	/* solar roof data structure */
 			var datalist_roof=[];
-			datalist_roof[0] = new dataDetailobj('1','Total Power',fields[1],'kWh');
-			datalist_roof[1] = new dataDetailobj('2','Current Power',fields[2],'W');
-			
+			datalist_roof[0] = new dataDetailobj('1','Total Generating Capacity',fields[1],'kWh');
+			datalist_roof[1] = new dataDetailobj('2','Total Active Power',fields[6],'W');
+			datalist_roof[2] = new dataDetailobj('3','Today Generating Capacity',fields[1]-fields_first[1],'khW');
+			datalist_roof[3] = new dataDetailobj('4','Average irradiance',fields[32],'W/m2');
+			datalist_roof[4] = new dataDetailobj('5','Average Cell Temp',fields[33],'C');
+			/* end */
+
 	  	  	var result = new dataobj("Solar Roof",fields[0],datalist_roof);
 	  	  	db.solarroof.remove({});
    	  		db.solarroof.insert(result);
-	    	console.log(result);
+	    	console.log('GET Solar Roof @'+fields[0]);
 		});
 
 
--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var mongojs = require('./db');
 //var mysql = require('mysql');
+
 var dateFormat = require('dateformat');
 var express = require('express');
 var app = express();
@@ -10,15 +11,43 @@ var db = mongojs.connect;
 
 app.use(express.static('public'));
 
+var kco =  0.517;  // 0.517 tonCO2/MWh
+var kpt = 1.25;    // tCO2 = 200
+
 // Routing
 app.get('/sp',function (req,res){
 
    	var db = mongojs.connect;
 	db.solarplant.findOne({plant:"Solar Plant"},function(err,doc){
+		doc['data'].forEach(function (item) {
+			if (item.oid == '4222124661211148')	{
+				cov = item.value*kco;
+				console.log(cov);
+
+				co2 = {'oid':'1', name:'CO2', value: cov, unit: 'Ton'}
+			}		
+		});
+		ptv = cov*200/kpt;
+
+		pt = {'oid':'2', name:'Plant', value: ptv, unit: 'EA'}
+		
+		doc['data'][20]=co2;
+		doc['data'][21]=pt;
 		res.json(doc);
-		console.log(doc.lastmodified);
+		console.log(doc.lastmodified + ': Solar Plant is requested.');
 	});
 });
+
+// Routing
+app.get('/sr',function (req,res){
+
+   	var db = mongojs.connect;
+	db.solarroof.findOne({plant:"Solar Roof"},function(err,doc){
+		res.json(doc);
+		console.log(doc.lastmodified + ': Solar Roof is requested.');
+	});
+});
+
 
 app.get('/sp/:oid',function (req,res){
    	var db = mongojs.connect;
